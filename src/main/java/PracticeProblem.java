c35373017a40a481d83f2ca289e4c4f6af6875e1
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-18 15:16:46 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,55 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+public static String pluralize(String word){
+if (word.toLowerCase().endsWith("ey")) {
+	return "eys";
+}
+else if (word.toLowerCase().endsWith("y")){
+	return "ies";
+}
 
+else if(word.toLowerCase().endsWith("ife")){
+	return "ives";
+}
+else{
+	return "s";
+}
+}
+
+public static int min(int num, int num2, int num3){
+	if (num > num2){
+			if(num2 > num3){
+return num3;
+			}
+			else{
+				return num2;
+			}
+	}
+	else{
+		if(num > num3){
+		return num3;
+					}
+					else{
+						return num;
+					}
+			}
+}
+
+public static boolean isLeapYear(int year){
+	if (year % 4 == 0){
+		if(year % 100 == 0){
+			if(year % 400 == 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return true;
+		}
+	}
+	return false;
+}
 }
